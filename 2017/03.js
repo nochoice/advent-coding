65aa27c472aa8f46390b5906dd3917af91e3fe84
Roman Kora <roman.kora@gmail.com>
2017-12-07 21:49:46 +0100

    Captcha + Spiral

--- 2017/03.js
+++ 2017/03.js
@@ -1,17 +1,21 @@
 clear();
-sideSize = (num) => {
+const sideSize = (num) => {
     let val = Math.ceil(Math.sqrt(num));
 
     if (val % 2 === 0) return Math.ceil(Math.sqrt(num)) + 1;
     else return Math.ceil(Math.sqrt(num));
 };
 
-middle = (num) => {
+const middle = (num) => {
     let size = sideSize(num);
-    
+    let center = Math.floor(size/2);
+    return ({
+        x: center,
+        y: center
+    });
 }
 
-ranges = (num) => {
+const ranges = (num) => {
     let sideMax = sideSize(num);
     let sideMin = sideMax - 2; 
 
@@ -21,11 +25,47 @@ ranges = (num) => {
     }
 }
 
-isBetween = (num, ranges) => {
+const position = (num) => {
+    let size = sideSize(num);
+    let ran = ranges(num);
+    let x = size - 1, y = size - 1;
+
+    for(let i = ran.max; i >= ran.min; i--) {
+
+        if(i === num) break;
+        if(x>0 && y === size-1) {x--; continue};
+
+        if(x === 0 && y > 0) {y--; continue} ;
+        if(y === 0 && (x >= 0 && x < size-1)) {x++; continue};
+        if(x === size - 1) {y++; continue};
+        
+    }
+
+    return {
+        x: x,
+        y: y
+    }
+}
+
+const isBetween = (num, ranges) => {
     return (num >= ranges.min && num <= ranges.max);
 }
 
-num = 962;
+num = 347991;
 
-console.log(ranges(num));
-console.log(isBetween(num, ranges(num)));
+// console.log(position(25));
+// console.log(position(23));
+// console.log(position(21));
+// console.log(position(19));
+// console.log(position(17));
+// console.log(position(15));
+// console.log(position(13));
+// console.log(position(11));
+// console.log(position(10));
+
+const numPosition = position(num);
+const middlePosition = middle(num);
+
+steps = Math.abs(numPosition.x - middlePosition.x) + Math.abs(numPosition.y - middlePosition.y)
+console.log(steps);
+
